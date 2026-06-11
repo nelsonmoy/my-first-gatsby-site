b4a8a4a8d3940b3ce190b538faa1dc26f46a54a6
Nelson Manuel Ordaz Yglesias <nelsonmanuel@outlook.com>
2026-06-11 00:50:37 -0600

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Greeting from "../components/greeting"
 
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <p>I'm a software developer with a passion for learning new technologies and sharing my knowledge with others. In my free time, I enjoy hiking, cooking, and spending time with my family.</p>
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import { Link } from 'gatsby'
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to my Gatsby site!">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <p>@Estudiando con Nelson.</p>
+      <p><Link to="https://www.youtube.com/@EstudiandoConNelson">@EstudiandoconNelson</Link></p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpg"
+      />
     </Layout>
   )
 }
